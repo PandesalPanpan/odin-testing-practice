6144ebcd4efd3f0996fa09b9d4a776320084aff4
PandesalPanpan <petermarticio@gmail.com>
2025-09-27 09:31:12 +0800

    Test: add, subtract, divide, multiply method test if exists

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -7,3 +7,14 @@ const calculator = new Calculator();
 it('instance of Calculator class', () => {
     expect(calculator instanceof Calculator).toBe(true);
 })
+
+it('add, subtract, divide, multiply exist', () => {
+    expect(calculator.add).toBeDefined();
+    expect(calculator.subtract).toBeDefined();
+    expect(calculator.divide).toBeDefined();
+    expect(calculator.multiply).toBeDefined();
+});
+
+it('add 5+10 = 15', () => {
+    expect(calculator.add(5+10)).toBe(15);
+})
