b9eae5e6d255d90c9e7d4d4e78c7181abe922b53
PandesalPanpan <petermarticio@gmail.com>
2025-09-27 11:02:08 +0800

    Feat: caesarCipher functionality

--- src/caesar-cipher.js
+++ src/caesar-cipher.js
@@ -1,3 +1,21 @@
-export default caesarCipher = () => {
-    
+export default caesarCipher = (string, key) => {
+    const alphabet = [
+        'a','b','c','d','e','f','g','h','i','j','k',
+        'l','m','n','o','p','q','r','s','t','u','v',
+        'w','x','y','z'];
+    let result = '';
+    for (let i = 0; i < string.length; i++) {
+        const char = string[i].toLowerCase();
+        const index = alphabet.indexOf(char);
+        let cipherChar = string[i];
+        if (index !== -1) {
+            cipherChar = alphabet[key+index] ?? alphabet[(key + index) - alphabet.length]
+        }
+        if (string[i] == string[i].toUpperCase()) {
+            cipherChar = cipherChar.toUpperCase();
+        }
+        
+        result += cipherChar;
+    }
+    return result;
 }
